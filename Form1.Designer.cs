869e837147893853e656035cc10d6355d74a6786
sveter2001 <sveter2001@mail.ru>
2022-07-03 03:27:07 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Untitled_masterpiece
+{
+    partial class Untitled_masterpiece
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Start = new System.Windows.Forms.Button();
+            this.button_Choose_level = new System.Windows.Forms.Button();
+            this.button_Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(890, 12);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(358, 124);
+            this.button_Start.TabIndex = 0;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // button_Choose_level
+            // 
+            this.button_Choose_level.Location = new System.Drawing.Point(890, 142);
+            this.button_Choose_level.Name = "button_Choose_level";
+            this.button_Choose_level.Size = new System.Drawing.Size(358, 124);
+            this.button_Choose_level.TabIndex = 1;
+            this.button_Choose_level.Text = "Choose level";
+            this.button_Choose_level.UseVisualStyleBackColor = true;
+            // 
+            // button_Exit
+            // 
+            this.button_Exit.Location = new System.Drawing.Point(890, 272);
+            this.button_Exit.Name = "button_Exit";
+            this.button_Exit.Size = new System.Drawing.Size(358, 124);
+            this.button_Exit.TabIndex = 2;
+            this.button_Exit.Text = "Exit";
+            this.button_Exit.UseVisualStyleBackColor = true;
+            this.button_Exit.Click += new System.EventHandler(this.button_Exit_Click);
+            // 
+            // Untitled_masterpiece
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(1260, 677);
+            this.Controls.Add(this.button_Exit);
+            this.Controls.Add(this.button_Choose_level);
+            this.Controls.Add(this.button_Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Untitled_masterpiece";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Button button_Choose_level;
+        private System.Windows.Forms.Button button_Exit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Untitled_masterpiece
+{
+    public partial class Untitled_masterpiece : Form
+    {
+        public Untitled_masterpiece()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            button_Start.Enabled = false;
+            button_Start.Visible = false;
+            button_Choose_level.Enabled = false;
+            button_Choose_level.Visible = false;
+            button_Exit.Enabled = false;
+            button_Exit.Visible = false;
+
+        }
+    }
+}
