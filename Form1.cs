a2d0a08949dfbdb2859a712146b8cf5f012a1c96
sveter2001 <sveter2001@mail.ru>
2022-07-14 14:10:28 +0300

    fixed animation final

--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,7 @@ namespace Untitled_masterpiece
         Image playerImg_R;
         private int currFrame = 0;
         private int currAnimation = 1;
+        private bool isPresedAnyKey = false;
         public Untitled_masterpiece()
         {
             InitializeComponent();
@@ -34,11 +35,28 @@ namespace Untitled_masterpiece
             timer1.Start();
 
             this.KeyDown += new KeyEventHandler(keyboard);
+            this.KeyUp += new KeyEventHandler(freeKeyb);
 
 
 
 
+        }
 
+        private void freeKeyb(object sender, KeyEventArgs e)
+        {
+            isPresedAnyKey = false;
+            if (currAnimation == 1)
+            {
+                currFrame = 1;
+                currAnimation = 3;
+            }
+                
+            if (currAnimation == 2)
+            {
+                currFrame = 1;
+                currAnimation = 4;
+            }
+               
         }
 
         private void keyboard(object sender, KeyEventArgs e)
@@ -47,23 +65,35 @@ namespace Untitled_masterpiece
             {
                 case "A":
                     currAnimation = 1;
+                    pictureBox1.Location = new Point(pictureBox1.Location.X - 1, pictureBox1.Location.Y);
                     break;
                 case "D":
                     currAnimation = 2;
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + 1, pictureBox1.Location.Y);
                     break;
 
             }
+            if (e.KeyCode.ToString() == "A" || e.KeyCode.ToString() == "D")
+            isPresedAnyKey = true;
         }
 
         private void update(object sender, EventArgs e)
         {
-            playAnimation();
+            if (isPresedAnyKey)
+            {
+                playAnimationMove();
+            }
+            else
+            {
+                playAnimationIdle();
+            }
+
             if (currFrame == 9)
                 currFrame = 0;
             currFrame++;
         }
 
-        private void playAnimation()
+        private void playAnimationMove()
         {
             if (currAnimation == 1)
             {
@@ -73,11 +103,30 @@ namespace Untitled_masterpiece
                 pictureBox1.Size = new Size(128, 128);
                 pictureBox1.Image = part;
             }
-            else
+            if (currAnimation == 2)
             {
                 Image part = new Bitmap(128, 128);
                 Graphics g = Graphics.FromImage(part);
                 g.DrawImage(playerImg_R, 0, 0, new Rectangle(new Point(1280 - 128* currFrame, 128 * 2), new Size(128, 128)), GraphicsUnit.Pixel);
+                pictureBox1.Size = new Size(128, 128);
+                pictureBox1.Image = part;
+            }
+        }
+        private void playAnimationIdle()
+        {
+            if (currAnimation == 3)
+            {
+                Image part = new Bitmap(128, 128);
+                Graphics g = Graphics.FromImage(part);
+                g.DrawImage(playerImg_L, 0, 0, new Rectangle(new Point(128 * currFrame, 128 * 0), new Size(128, 128)), GraphicsUnit.Pixel);
+                pictureBox1.Size = new Size(128, 128);
+                pictureBox1.Image = part;
+            }
+            if (currAnimation == 4)
+            {
+                Image part = new Bitmap(128, 128);
+                Graphics g = Graphics.FromImage(part);
+                g.DrawImage(playerImg_R, 0, 0, new Rectangle(new Point(1280 - 128 * currFrame, 128 * 0), new Size(128, 128)), GraphicsUnit.Pixel);
                 pictureBox1.Size = new Size(128, 128);
                 pictureBox1.Image = part;
             }
@@ -103,14 +152,6 @@ namespace Untitled_masterpiece
 
 
         }
-
-
-        public class Mumu
-        {
-            
-            
-        }
-
        
     }
 }
