3af71d14f510390755bd8a64f3df5c098456457a
sveter2001 <sveter2001@mail.ru>
2022-07-14 13:33:35 +0300

    fixed animation

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,9 +29,13 @@ namespace Untitled_masterpiece
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button_Start = new System.Windows.Forms.Button();
             this.button_Choose_level = new System.Windows.Forms.Button();
             this.button_Exit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button_Start
@@ -63,10 +67,19 @@ namespace Untitled_masterpiece
             this.button_Exit.UseVisualStyleBackColor = true;
             this.button_Exit.Click += new System.EventHandler(this.button_Exit_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(303, 208);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 130);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
             // Untitled_masterpiece
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.ClientSize = new System.Drawing.Size(1260, 677);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button_Exit);
             this.Controls.Add(this.button_Choose_level);
             this.Controls.Add(this.button_Start);
@@ -75,6 +88,7 @@ namespace Untitled_masterpiece
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -84,6 +98,8 @@ namespace Untitled_masterpiece
         private System.Windows.Forms.Button button_Start;
         private System.Windows.Forms.Button button_Choose_level;
         private System.Windows.Forms.Button button_Exit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,74 @@ namespace Untitled_masterpiece
 {
     public partial class Untitled_masterpiece : Form
     {
+        Image playerImg_L;
+        Image playerImg_R;
+        private int currFrame = 0;
+        private int currAnimation = 1;
         public Untitled_masterpiece()
         {
             InitializeComponent();
+            // Используйте двойную буферизацию для уменьшения мерцания.
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+            playerImg_L = new Bitmap("D:\\slime.png");
+            playerImg_R = new Bitmap("D:\\slimeR.png");
+
+
+
+            timer1.Interval = 100;
+            timer1.Tick += new EventHandler(update);
+            timer1.Start();
+
+            this.KeyDown += new KeyEventHandler(keyboard);
+
+
+
+
+
+        }
+
+        private void keyboard(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "A":
+                    currAnimation = 1;
+                    break;
+                case "D":
+                    currAnimation = 2;
+                    break;
+
+            }
+        }
+
+        private void update(object sender, EventArgs e)
+        {
+            playAnimation();
+            if (currFrame == 9)
+                currFrame = 0;
+            currFrame++;
+        }
+
+        private void playAnimation()
+        {
+            if (currAnimation == 1)
+            {
+                Image part = new Bitmap(128, 128);
+                Graphics g = Graphics.FromImage(part);
+                g.DrawImage(playerImg_L, 0, 0, new Rectangle(new Point(128 * currFrame, 128 * 2), new Size(128, 128)), GraphicsUnit.Pixel);
+                pictureBox1.Size = new Size(128, 128);
+                pictureBox1.Image = part;
+            }
+            else
+            {
+                Image part = new Bitmap(128, 128);
+                Graphics g = Graphics.FromImage(part);
+                g.DrawImage(playerImg_R, 0, 0, new Rectangle(new Point(1280 - 128* currFrame, 128 * 2), new Size(128, 128)), GraphicsUnit.Pixel);
+                pictureBox1.Size = new Size(128, 128);
+                pictureBox1.Image = part;
+            }
         }
 
         private void button_Exit_Click(object sender, EventArgs e)
@@ -22,7 +88,7 @@ namespace Untitled_masterpiece
             Application.Exit();
         }
 
-        private void button_Start_Click(object sender, EventArgs e)
+        public void button_Start_Click(object sender, EventArgs e)
         {
             button_Start.Enabled = false;
             button_Start.Visible = false;
@@ -31,6 +97,20 @@ namespace Untitled_masterpiece
             button_Exit.Enabled = false;
             button_Exit.Visible = false;
 
+
+
+
+
+
         }
+
+
+        public class Mumu
+        {
+            
+            
+        }
+
+       
     }
 }
